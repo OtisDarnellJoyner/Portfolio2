3477518b8987da6e27f3427fcd068e40f524ce93
darj0008 <darj0008@gmail.com>
2017-04-26 18:38:01 -0400

    Reconfigured folder structre

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,11 @@ var sass = require('gulp-sass');
 var bs = require('browser-sync');
 
 gulp.task('pug', function(){
-    return gulp.src('./src/*.pug')
+    return gulp.src('./src/index.pug')
     .pipe(pug({
         pretty: true
     }))
-    .pipe(gulp.dest('./views'))
-	.pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./src'));
 });
 
 gulp.task('pug-watch',['pug'] , function(done){
@@ -18,9 +17,9 @@ gulp.task('pug-watch',['pug'] , function(done){
 });
 
 gulp.task('sass', function(){
-	return gulp.src('./sass/*.sass')
+	return gulp.src('./src/./sass/*.sass')
 	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./css'))
+	.pipe(gulp.dest('./src/css'))
 });
 
 gulp.task('sass-watch',['sass'], function(done){
@@ -33,11 +32,11 @@ gulp.task('sass-watch',['sass'], function(done){
 gulp.task('default', ['pug-watch', 'sass-watch'], function(){
 	bs.init({
 		server: {
-			baseDir: "./",
+			baseDir: "./src",
 			index: "index.html"
 		}
 	});
 
 	gulp.watch('./src/*.pug', ['pug-watch']);
-	gulp.watch('./sass/*.sass', ['sass-watch']);
+	gulp.watch('./src/sass/*.sass', ['sass-watch']);
 });
